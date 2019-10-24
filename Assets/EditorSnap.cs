ab7147569cf7a1328c435e84ca92f633cd21e1d7
srb76 <bonnersa@oregonstate.edu>
2019-10-24 12:49:06 -0700

    Added snapping and coordinate labelling to cube prefab

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] [Tooltip("How far the object will snap to in the editor")] float gridSize = 10f;
+    [SerializeField] float yPos = 0f;
+    TextMesh textMesh;
+    //string xCoord, zCoord;
+
+    void Awake()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = yPos;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+    
+        transform.position = snapPos;
+
+        textMesh.text = snapPos.x / 10f + "," + snapPos.z / 10f;
+    }
+}
