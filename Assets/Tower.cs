3dcae60b4497b6c30cff8962d8883c1c27c92b96
srb76 <bonnersa@oregonstate.edu>
2020-06-26 12:02:12 -0700

    Added skeleton tower class

---
+++ Assets/Tower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Waypoint attached;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set location (temporary)
+        transform.position = attached.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
